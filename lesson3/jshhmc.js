72fe0bcd2905288f8d0051652b6bb4a3e94e6b1c
espnal <78657757+espnal@users.noreply.github.com>
2022-01-27 01:39:27 -0400

    lesson3 new changes

---
+++ lesson3/jshhmc.js
@@ -0,0 +1,5 @@
+const lastmod = document.querySelector("#lastmod");
+lastmod.textContent = `Last update: ${document.lastModified}`;
+
+const naming = document.querySelector("#naming");
+naming.textContent = `© 2022 | Roguin P. Espinal | Republica Dominicana`
