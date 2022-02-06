5e87cc2853fd789c570ae41b898f8f3e6d5013c2
espnal <78657757+espnal@users.noreply.github.com>
2022-02-06 17:59:22 -0400

    latest

--- lesson5/js.js
+++ lesson5/js.js
@@ -1,24 +1,43 @@
-const list = document.querySelector('ul');
-const input = document.querySelector('input');
-const button = document.querySelector('button');
+const input = document.querySelector("input");
+const addBtn = document.querySelector(".btn-add");
+const ul = document.querySelector("ul");
+const empty = document.querySelector(".empty");
 
-button.onclick = function() {
-    let myItem = input.value;
-    input.value = '';
+addBtn.addEventListener("click", (e) => {
+    e.preventDefault();
 
-    const listItem = document.createElement('li');
-    const listText = document.createElement('span');
-    const listBtn = document.createElement('button');
+    const text = input.value;
 
-    listItem.appendChild(listText);
-    listText.textContent = myItem;
-    listItem.appendChild(listBtn);
-    listBtn.textContent = '❌';
-    list.appendChild(listItem);
+    if (text !== "") {
+        const li = document.createElement("li");
+        const p = document.createElement("p");
+        p.textContent = text;
 
-    listBtn.onclick = function(e) {
-        list.removeChild(listItem);
+        li.appendChild(p);
+        li.appendChild(addDeleteBtn());
+        ul.appendChild(li);
+
+        input.value = "";
+        empty.style.display = "none";
     }
+});
 
-    input.focus();
+function addDeleteBtn() {
+    const deleteBtn = document.createElement("button");
+
+    deleteBtn.textContent = "X";
+    deleteBtn.className = "btn-delete";
+
+    deleteBtn.addEventListener("click", (e) => {
+        const item = e.target.parentElement;
+        ul.removeChild(item);
+
+        const items = document.querySelectorAll("li");
+
+        if (items.length === 0) {
+            empty.style.display = "block";
+        }
+    });
+
+    return deleteBtn;
 }
